2437da01c57c61ba26622d858ff2c52a6a707821
EyoabA1 <eyoabaddisu123@gmail.com>
2025-02-20 23:46:00 +0000

    Eyoab A ANswer to first coding problem

---
+++ EyoabA.py
@@ -0,0 +1,19 @@
+# Write a function to reverse a string.
+# It is asking us to write code that takes input and outputs it in reverse.
+#Edge Cases- Lists, Intergs, etc.
+
+
+# Pesudo code
+# def (reverse function I will find in docs.)
+    #return reverse
+
+# input_string = "Hello"
+# reversed string = input_string
+# print input string (reversed)
+
+def reverse_string(reverse):
+    return reverse[::-1]
+
+input_string = "Hello"
+reversed_string = reverse_string(input_string)
+print(reversed_string)
